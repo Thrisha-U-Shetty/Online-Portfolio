73c7e1add5cce64bdb4330e9dd6d030d7cc83a1b
Thrisha-U-Shetty <thrishashetty009@gmail.com>
2025-08-21 22:18:13 +0530

    addec a profile

---
+++ my-app/src/Components/HeroModels/HeroExperience.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import thrishaImg from "../assets/thrisha.jpg";
+
+
+const HeroExperience = () => {
+  return (
+    <>
+      <div>
+      <img src={thrishaImg} alt="image"></img>
+    </div>
+    </>
+  )
+}
+
+export default HeroExperience
--- my-app/src/Sections/Hero.jsx
+++ my-app/src/Sections/Hero.jsx
@@ -1,40 +1,60 @@
 import React from 'react'
 import { words } from '../Constants'
 import Button from '../Components/Button'
+import thrishaImg from "../assets/thrisha.jpg";
+
 const Hero = () => {
     return (
         <section id="hero" className='relative overflow-hidden'>
+            {/* Background */}
             <div className='absolute top-0 left-0 z-10'>
-                <img src='/images/bg.png' alt='backgound ' />
+                <img src='/images/bg.png' alt='background ' />
             </div>
-            <div className='hero-layout'>
-                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-                    <div className='flex flex-col gap-7'>
-                        <div className='hero-text'>
-                            <h1>
-                                Shaping
-                                <span className='slide'>
-                                    <span className='wrapper'>
-                                        {words.map((word) => (
-                                            <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
-                                                <img src={word.imgPath}
-                                                    alt={word.text}
-                                                    className='xl:size-12 md:size-10 size7 md:p-2 p-1 rounded-full bg-white-50' />
-                                                <span>{word.text}</span>
-                                            </span>
-                                        ))}
-                                    </span>
+
+            {/* Hero Layout: Text on Left, Image on Right */}
+            <div className='hero-layout flex flex-col md:flex-row items-center justify-between md:px-20 px-5 relative z-20'>
+
+                {/* Left Side - Text */}
+                <header className='flex flex-col justify-center md:w-1/2 w-full gap-7'>
+                    <div className='hero-text'>
+                        <h1>
+                            Shaping
+                            <span className='slide'>
+                                <span className='wrapper'>
+                                    {words.map((word) => (
+                                        <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                            <img src={word.imgPath}
+                                                alt={word.text}
+                                                className='xl:size-12 md:size-10 size7 md:p-2 p-1 rounded-full bg-white-50' />
+                                            <span>{word.text}</span>
+                                        </span>
+                                    ))}
                                 </span>
-                            </h1>
-                            <h1>into Real Projects</h1>
-                            <h1>that Deliver Results</h1>
-                        </div>
-                        <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>Hii,I am Thrisha U Shetty,a developer with a passion for code and designs.</p>
-                        <Button className="md:w-80 md:h-16 w-60 h-12"
-                        id="button"
-                        text="See my work"/>
+                            </span>
+                        </h1>
+                        <h1>into Real Projects</h1>
+                        <h1>that Deliver Results</h1>
                     </div>
+                    <p className='text-white-50 md:text-xl'>
+                        Hii, I am Thrisha U Shetty, a developer with a passion for code and designs.
+                    </p>
+                    <Button
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        id="button"
+                        text="See my work"
+                    />
                 </header>
+
+                {/* Right Side - Image */}
+                <figure className="md:w-1/2 w-full flex justify-center mt-10 md:mt-0">
+                    <div className='p-2 rounded-full shadow-lg border-2 border-gray-200'>
+                        <img
+                            src={thrishaImg}
+                            alt="Thrisha U Shetty"
+                            className="w-70 h-70 object-cover rounded-full border-4 border-white shadow-md"
+                        />
+                    </div>
+                </figure>
             </div>
         </section>
     )
